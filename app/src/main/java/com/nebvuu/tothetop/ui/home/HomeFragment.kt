114c78e0c6a4e26ab8266a62a5ea0d4905eff596
ShoneBL <nebojsashonevujic@gmail.com>
2022-05-28 13:38:06 +0200

    - home screen UI

--- app/src/main/java/com/nebvuu/tothetop/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nebvuu/tothetop/ui/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.nebvuu.tothetop.ui.home
 
+import android.graphics.Outline
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.nebvuu.tothetop.R
 import com.nebvuu.tothetop.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,16 +27,30 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        //val textView: TextView = binding.textHome
+        //val textTest: TextView = binding.test
+        val ivProfileImage: ImageView = binding.ivProfileImage
+
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            //textView.text = it
         }
+
+        homeViewModel.textTest.observe(viewLifecycleOwner) {
+            //textTest.text = it
+        }
+
+        homeViewModel.image.observe(viewLifecycleOwner){
+            ivProfileImage.load(R.drawable.profile_user){
+                crossfade(true)
+                transformations(CircleCropTransformation())
+            }
+        }
+
         return root
     }
 
--- app/src/main/java/com/nebvuu/tothetop/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/nebvuu/tothetop/ui/home/HomeViewModel.kt
@@ -1,13 +1,27 @@
 package com.nebvuu.tothetop.ui.home
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nebvuu.tothetop.R
 
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
+    private val _test = MutableLiveData<String>().apply {
+        value = "This is test"
+    }
+
+
+    private val _image = MutableLiveData<Int>().apply {
+        value = R.drawable.profile_user
+    }
+
     val text: LiveData<String> = _text
+    val textTest: LiveData<String> = _test
+
+    val image:LiveData<Int> = _image
 }
